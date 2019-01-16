33fe7f77239e0abb8c3d828391a4b1074951842c
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2019-01-16 17:28:21 +0100

    1.4.1

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-powershell -NoExit -ExecutionPolicy Unrestricted -Command .\build.ps1 -buildVersion '1.4.0' -signtool 'signtool sign /sha1 6FDEC9DFCFE59E6BAEE64B7ED97F00E120E70D97 $p'
+powershell -NoExit -ExecutionPolicy Unrestricted -Command .\build.ps1 -buildVersion '1.4.1' -signtool 'signtool sign /sha1 6FDEC9DFCFE59E6BAEE64B7ED97F00E120E70D97 $p'
