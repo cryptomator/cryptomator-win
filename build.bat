fc33eb98891ddab0b4d227686a9bc0f052ef2b98
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2020-11-10 13:30:04 +0100

    build with adoptopenjdk 14.0.2

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-set JAVA_HOME=C:\Program Files\AdoptOpenJDK\jdk-14.0.1.7-hotspot
+set JAVA_HOME=C:\Program Files\AdoptOpenJDK\jdk-14.0.2.12-hotspot
 echo BEFORE WE START: You need to manually update resources/resourcehacker/cryptomator.rc!
 set /p GIT_TAG="Ready? Enter the Cryptomator version (as in the git tag): " || Set GIT_TAG=snapshot
 powershell -NoExit -ExecutionPolicy Unrestricted -Command .\build.ps1 -upstreamVersion %GIT_TAG%
